5fea2072983f431b72316e822d09d17768131be6
Yashovardhn <yashovardhn@gmail.com>
2024-07-21 17:09:36 +0530

    ok

--- file_sharing_system/files/urls.py
+++ file_sharing_system/files/urls.py
@@ -1,5 +1,6 @@
 # file_sharing_system/urls.py
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
 
 urlpatterns = [
@@ -9,5 +10,8 @@ urlpatterns = [
     path('upload-file/', views.upload_file, name='upload-file'),
     path('list-files/', views.list_files, name='list-files'),
     path('download-file/<int:pk>/', views.download_file, name='download-file'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
 
